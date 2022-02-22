fe765bf75d0ff1e1750a783e08ac1be365f6e677
Isaac <icpereiradoprado@gmail.com>
2022-02-22 20:42:40 -0300

    Exercício: Calculadora Simples

---
+++ CalculadoraSimples/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CalculadoraSimples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numero1;
+            double numero2;
+
+            Console.WriteLine("Digite o primeiro número: ");
+            numero1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("==========================\n");
+            Console.WriteLine("Digite o segundo número: ");
+            numero2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("==========================\n");
+
+            Console.WriteLine($"Soma: {numero1} + {numero2} = " + String.Format("{0:0.00}", numero1 + numero2));
+            Console.WriteLine($"Subtração: {numero1} - {numero2} = " + String.Format("{0:0.00}", numero1 - numero2));
+            Console.WriteLine($"Multiplicação: {numero1} * {numero2} = " + String.Format("{0:0.00}", numero1 * numero2));
+            Console.WriteLine($"Divisão: {numero1} / {numero2} = " + String.Format("{0:0.00}", numero1 / numero2));
+
+        }
+    }
+}
